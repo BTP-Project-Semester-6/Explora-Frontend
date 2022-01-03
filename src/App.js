4eb50aa2a0ee19c472e510e2eac6e1252a966e25
Nalin Agrawal <54065357+nalin-programmer@users.noreply.github.com>
2022-01-03 15:31:29 +0530

    challenge and buddy page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./Pages/Admin/admin";
+import Buddy from "./Pages/buddy/buddy";
 import Challenge from "./Pages/Challenge/challenge";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path="/admin" element={<Admin />} />
         <Route path="/challenge" element={<Challenge />} />
+        <Route path="/buddy" element={<Buddy />} />
       </Routes>
     </Router>
   );
--- src/Pages/Challenge/challenge.js
+++ src/Pages/Challenge/challenge.js
@@ -15,7 +15,7 @@ export default function Challenge() {
     <div
       style={{
         width: "100%",
-        minHeight: "700px",
+        height: "80vh",
         margin: "0",
         padding: "0",
       }}
@@ -52,186 +52,108 @@ export default function Challenge() {
           </IconButton>
         </Paper>
       </div>
+
       <div
+        className="buddy-scroll"
         style={{
-          width: "100%",
-          minHeight: "700px",
-          margin: "0",
+          width: "75%",
           padding: "0",
-          display: "flex",
+          display: "vertical",
+          margin: "auto",
+          marginTop: "0",
+          padding: "auto",
+          overflowY: "auto",
+          overflowX: "hidden",
+          height: "90vh",
         }}
       >
-        <div
-          style={{
-            width: "30%",
-            minHeight: "100%",
-            padding: "0",
-            display: "vertical",
-            margin: "auto",
-            marginTop: "0",
-            padding: "auto",
-          }}
-        >
-          <div class="twelve">
-            <h1>GUIDES</h1>
-          </div>
+        <div class="twelve">
+          <h1>CHALLENGES</h1>
+        </div>
+        <div className="horizontal-card">
           <div
             style={{
-              marginTop: "20px",
-              marginLeft: "20px",
+              width: "150px",
+              height: "100%",
+              padding: "auto",
+              marginTop: "30px",
             }}
           >
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image="https://upload.wikimedia.org/wikipedia/commons/1/15/Gwalior_1.JPG"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Ramesh
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Guide in gwalior fort. Rs.200 per trip. All seven forts and 3
-                  temples inside gwalior fort.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">VIEW PROFILE</Button>
-              </CardActions>
-            </Card>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/1/15/Gwalior_1.JPG"
+              alt=""
+            />
           </div>
+          <div className="horizontal-card-body" style={{ width: "100%" }}>
+            <div className="orders" style={{ width: "100%" }}>
+              <div>
+                <p className="name" style={{ float: "left" }}>
+                  challenge name
+                </p>
+              </div>
+              <div style={{ float: "right" }}>⏱️24 hours 🗺️Gwalior</div>
+            </div>
+            <div>
+              <p className="order">
+                The Gwalior Fort (Gwāliiyar Qila) is a hill fort near Gwalior,
+                Madhya Pradesh, India. The fort has existed at least since the
+                10th century, and the inscriptions and monuments found within
+                what is now the fort campus indicate that it may have existed as
+                early as the beginning of the 6th century. The modern-day fort,
+                consisting a defensive structure and two palaces was built by
+                Tomar Rajput ruler Man Singh Tomar.[1] The fort has been
+                controlled by a number of different rulers in its history.
+              </p>
+            </div>
 
-          <div
-            style={{
-              marginTop: "20px",
-              marginLeft: "20px",
-            }}
-          >
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image="https://upload.wikimedia.org/wikipedia/commons/1/15/Gwalior_1.JPG"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Ramesh
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Guide in gwalior fort. Rs.200 per trip. All seven forts and 3
-                  temples inside gwalior fort.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">VIEW PROFILE</Button>
-              </CardActions>
-            </Card>
+            <div className="profile">
+              <Button variant="contained" href="#contained-buttons">
+                Start Challenge
+              </Button>
+            </div>
           </div>
         </div>
-        <div
-          style={{
-            width: "70%",
-            minHeight: "100%",
-            padding: "0",
-            display: "vertical",
-            margin: "auto",
-            marginTop: "0",
-            padding: "auto",
-          }}
-        >
-          <div class="twelve">
-            <h1>CHALLENGES</h1>
+
+        <div className="horizontal-card">
+          <div
+            style={{
+              width: "150px",
+              height: "100%",
+              padding: "auto",
+              marginTop: "30px",
+            }}
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/1/15/Gwalior_1.JPG"
+              alt=""
+            />
           </div>
-          <div className="horizontal-card">
-            <div
-              style={{
-                width: "150px",
-                height: "100%",
-                padding: "auto",
-                marginTop: "30px",
-              }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/1/15/Gwalior_1.JPG"
-                alt=""
-              />
-            </div>
-            <div className="horizontal-card-body" style={{ width: "100%" }}>
-              <div className="orders" style={{ width: "100%" }}>
-                <div>
-                  <p className="name" style={{ float: "left" }}>
-                    challenge name
-                  </p>
-                </div>
-                <div style={{ float: "right" }}>⏱️24 hours 🗺️Gwalior</div>
-              </div>
+          <div className="horizontal-card-body" style={{ width: "100%" }}>
+            <div className="orders" style={{ width: "100%" }}>
               <div>
-                <p className="order">
-                  The Gwalior Fort (Gwāliiyar Qila) is a hill fort near Gwalior,
-                  Madhya Pradesh, India. The fort has existed at least since the
-                  10th century, and the inscriptions and monuments found within
-                  what is now the fort campus indicate that it may have existed
-                  as early as the beginning of the 6th century. The modern-day
-                  fort, consisting a defensive structure and two palaces was
-                  built by Tomar Rajput ruler Man Singh Tomar.[1] The fort has
-                  been controlled by a number of different rulers in its
-                  history.
+                <p className="name" style={{ float: "left" }}>
+                  challenge name
                 </p>
               </div>
-
-              <div className="profile">
-                <Button variant="contained" href="#contained-buttons">
-                  Start Challenge
-                </Button>
-              </div>
+              <div style={{ float: "right" }}>⏱️24 hours 🗺️Gwalior</div>
             </div>
-          </div>
-
-          <div className="horizontal-card">
-            <div
-              style={{
-                width: "150px",
-                height: "100%",
-                padding: "auto",
-                marginTop: "30px",
-              }}
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/1/15/Gwalior_1.JPG"
-                alt=""
-              />
+            <div>
+              <p className="order">
+                The Gwalior Fort (Gwāliiyar Qila) is a hill fort near Gwalior,
+                Madhya Pradesh, India. The fort has existed at least since the
+                10th century, and the inscriptions and monuments found within
+                what is now the fort campus indicate that it may have existed as
+                early as the beginning of the 6th century. The modern-day fort,
+                consisting a defensive structure and two palaces was built by
+                Tomar Rajput ruler Man Singh Tomar.[1] The fort has been
+                controlled by a number of different rulers in its history.
+              </p>
             </div>
-            <div className="horizontal-card-body" style={{ width: "100%" }}>
-              <div className="orders" style={{ width: "100%" }}>
-                <div>
-                  <p className="name" style={{ float: "left" }}>
-                    challenge name
-                  </p>
-                </div>
-                <div style={{ float: "right" }}>⏱️24 hours 🗺️Gwalior</div>
-              </div>
-              <div>
-                <p className="order">
-                  The Gwalior Fort (Gwāliiyar Qila) is a hill fort near Gwalior,
-                  Madhya Pradesh, India. The fort has existed at least since the
-                  10th century, and the inscriptions and monuments found within
-                  what is now the fort campus indicate that it may have existed
-                  as early as the beginning of the 6th century. The modern-day
-                  fort, consisting a defensive structure and two palaces was
-                  built by Tomar Rajput ruler Man Singh Tomar.[1] The fort has
-                  been controlled by a number of different rulers in its
-                  history.
-                </p>
-              </div>
 
-              <div className="profile">
-                <Button variant="contained" href="#contained-buttons">
-                  Start Challenge
-                </Button>
-              </div>
+            <div className="profile">
+              <Button variant="contained" href="#contained-buttons">
+                Start Challenge
+              </Button>
             </div>
           </div>
         </div>
