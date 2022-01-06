d32ec47fe0c10f5a099ddca4ecfb3c626786c1de
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-01-06 11:22:29 +0530

    getGuideByCityReducer Created

---
+++ src/reducers/guideReducer.js
@@ -0,0 +1,18 @@
+export const getBuddyByCityReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "GET_GUIDE_BY_CITY_REQUEST":
+      return { loading: true, guide: [] };
+    case "GET_GUIDE_BY_CITY_SUCCESS":
+      console.log(action.payload);
+      return { loading: false, success: true, guide: action.payload };
+    case "GET_GUIDE_BY_CITY_FAIL":
+      return {
+        loading: false,
+        success: false,
+        error: action.payload,
+        guide: [],
+      };
+    default:
+      return { loading: false, guide: [] };
+  }
+};
