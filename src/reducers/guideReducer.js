052049c137e4af36f1e4bf53de882977601f5a10
Nalin Agrawal <54065357+nalin-programmer@users.noreply.github.com>
2022-01-06 11:37:54 +0530

    getGuideByCityReducer created

--- src/reducers/guideReducer.js
+++ src/reducers/guideReducer.js
@@ -1,10 +1,10 @@
-export const getBuddyByCityReducer = (state = {}, action) => {
+export const getGuideByCityReducer = (state = {}, action) => {
   switch (action.type) {
     case "GET_GUIDE_BY_CITY_REQUEST":
       return { loading: true, guide: [] };
     case "GET_GUIDE_BY_CITY_SUCCESS":
       console.log(action.payload);
-      return { loading: false, success: true, guide: action.payload };
+      return { loading: false, success: true, guide: action.payload.data };
     case "GET_GUIDE_BY_CITY_FAIL":
       return {
         loading: false,
