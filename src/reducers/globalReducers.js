1472a5d68b5eb6972554ea2b7a6809ed80d400b1
Nalin Agrawal <54065357+nalin-programmer@users.noreply.github.com>
2022-01-06 11:11:36 +0530

    getbuddy by city reducer created with minor changes

--- src/reducers/globalReducers.js
+++ src/reducers/globalReducers.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import sampleChangeTheNumber from "./sampleReducer";
 import { getChallengeByCityReducer } from "./challengeReducer";
-
+import { getBuddyByCityReducer } from "./buddyReducer";
 const reducers = combineReducers({
   sampleChangeTheNumber,
   getChallengeByCityReducer,
+  getBuddyByCityReducer,
 });
 
 export default reducers;
