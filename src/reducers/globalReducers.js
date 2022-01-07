a12a7c3c1486774963a67aab454718b3431c19a5
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-01-07 12:14:26 +0530

    buddy page connected to backend

--- src/reducers/globalReducers.js
+++ src/reducers/globalReducers.js
@@ -2,12 +2,16 @@ import { combineReducers } from "redux";
 import sampleChangeTheNumber from "./sampleReducer";
 import { getChallengeByCityReducer } from "./challengeReducer";
 import { getBuddyByCityReducer } from "./buddyReducer";
-import { getGuideByCityReducer } from "./guideReducer";
+import {
+  getGuideByCityReducer,
+  getGuideAndBuddyByCityReducer,
+} from "./guideReducer";
 const reducers = combineReducers({
   sampleChangeTheNumber,
   getChallengeByCityReducer,
   getBuddyByCityReducer,
   getGuideByCityReducer,
+  getGuideAndBuddyByCityReducer,
 });
 
 export default reducers;
