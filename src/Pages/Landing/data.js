1ad009fee7fa3492ec793ef172757d51fb01818c
Prerit Kumar Jha <preritkrjha@gmail.com>
2022-01-08 16:00:52 +0530

    Landing page

---
+++ src/Pages/Landing/data.js
@@ -0,0 +1,57 @@
+import img1 from "../../img/2202447.jpg";
+import img2 from "../../img/5755637.jpg";
+import img3 from "../../img/2642062.jpg";
+import img4 from "../../img/3235342.jpg";
+import img5 from "../../img/4432213.jpg";
+import img6 from "../../img/4438372.jpg";
+
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "🌇 Social Travel Platform",
+  headline: "Your Travel Engagement!",
+  description:
+    "Explora is a social travel platform that comprises a unique blend of social media and a travel website. Conventional travel websites lack the social engagement of users.",
+  buttonLabel: "Get Started",
+  imgStart: "start",
+  img: img2,
+  alt: "Gift",
+  start: "",
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "🌇 Social Travel Platform",
+  headline: "Your Travel Engagement!",
+  description:
+    "Explora is a social travel platform that comprises a unique blend of social media and a travel website. Conventional travel websites lack the social engagement of users.",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img: img6,
+  alt: "Gift",
+  start: "",
+};
+
+export const homeObjThree = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "🌇 Social Travel Platform",
+  headline: "Your Travel Engagement!",
+  description:
+    "Explora is a social travel platform that comprises a unique blend of social media and a travel website. Conventional travel websites lack the social engagement of users.",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img: img3,
+  alt: "Gift",
+  start: "",
+};
