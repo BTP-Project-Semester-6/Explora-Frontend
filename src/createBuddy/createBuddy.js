c8b3d809c2c572109c826254845f6933fc55bb2b
Nalin Agrawal <54065357+nalin-programmer@users.noreply.github.com>
2022-01-18 21:00:26 +0530

    create buddy page completed

--- src/createBuddy/createBuddy.js
+++ src/createBuddy/createBuddy.js
@@ -13,12 +13,10 @@ import Navbar from "../Pages/navbar/navbar";
 
 import { styled } from "@mui/system";
 import { DatePicker } from "@mui/lab";
+import Toast from "../Components/Toast/toast";
+import { useDispatch, useSelector } from "react-redux";
+import { createGroup } from "../actions/buddyAction";
 export default function CreatBuddy() {
-  // const [age, setAge] = React.useState("");
-
-  // const handleChange = (event) => {
-  //   setAge(event.target.value);
-  // };
   const [groupMaxSize, setGroupMaxSize] = useState(1);
   const [city, setCity] = useState("");
   const [dateOfArrival, setDateOfArrival] = useState(new Date());
@@ -27,9 +25,11 @@ export default function CreatBuddy() {
   const [host, setHost] = useState("");
   const [hostID, setHostID] = useState("");
 
-  // const [user, setUser] = useState({});
-
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const result = useSelector((state) => state.createGroupReducer);
+  console.log(result);
   useEffect(() => {
     if (localStorage.getItem("token") === null) {
       navigate("/login");
@@ -51,6 +51,34 @@ export default function CreatBuddy() {
     }
   }, []);
 
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    if (hostID == "" || host == "") {
+      Toast("", "Please login to continue..", "", "");
+    } else if (
+      groupMaxSize &&
+      city &&
+      dateOfArrival &&
+      dateOfDeparture &&
+      description
+    ) {
+      Toast("", "", "Request Sent", "");
+      dispatch(
+        createGroup(
+          groupMaxSize,
+          city,
+          dateOfArrival,
+          dateOfDeparture,
+          description,
+          hostID,
+          hostID
+        )
+      );
+    } else {
+      Toast("", "", "", "Please fill all details");
+    }
+  };
+
   const myStyle = {
     backgroundImage: "url(/buddy.jpg)",
     height: "100vh",
@@ -60,25 +88,27 @@ export default function CreatBuddy() {
     backgroundPosition: "center",
     backgroundSize: "cover",
   };
+  Toast(result.message, result.error, "", "");
   return (
     <div className="creatBuddy-body">
       <div style={{ paddingTop: "4%" }} className="him">
         <div style={{ width: "60%" }} className="container glass-createBuddy">
-          <div style={{ width: "100%", margin: "auto" }} className="row">
-            <div style={{ width: "20%" }} class="form-group">
-              <label for="formGroupExampleInput2">
-                <b>Total Members</b>{" "}
-              </label>
-              <input
-                type="number"
-                value={groupMaxSize}
-                class="form-control"
-                id="formGroupExampleInput2"
-                placeholder="Members"
-                onChange={(e) => setGroupMaxSize(e.target.value)}
-              />
-            </div>
-            {/* <div style={{ width: "60%" }} class="form-group">
+          <form onSubmit={SubmitHandler}>
+            <div style={{ width: "100%", margin: "auto" }} className="row">
+              <div style={{ width: "20%" }} class="form-group">
+                <label for="formGroupExampleInput2">
+                  <b>Total Members</b>{" "}
+                </label>
+                <input
+                  type="number"
+                  value={groupMaxSize}
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Members"
+                  onChange={(e) => setGroupMaxSize(e.target.value)}
+                />
+              </div>
+              {/* <div style={{ width: "60%" }} class="form-group">
               <label for="formGroupExampleInput2">
                 <b>Add Members</b>
               </label>
@@ -89,76 +119,77 @@ export default function CreatBuddy() {
                 placeholder="AddMembers"
               />
             </div> */}
-            <div style={{ marginTop: "24px", width: "20%" }}>
-              {/* <button type="button" class="btn btn-outline-dark">
+              <div style={{ marginTop: "24px", width: "20%" }}>
+                {/* <button type="button" class="btn btn-outline-dark">
                 {" "}
                 <b>Add</b>
               </button> */}
+              </div>
             </div>
-          </div>
-          <div style={{ width: "98%", margin: "auto" }} class="form-group">
-            <label for="formGroupExampleInput2">
-              <b>City</b>
-            </label>
-            <input
-              type="text"
-              value={city}
-              class="form-control"
-              id="formGroupExampleInput2"
-              placeholder="City"
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
-
-          <div style={{ margin: "auto", marginTop: "24px" }} className="row">
-            <div style={{ width: "50%" }} class="form-group">
+            <div style={{ width: "98%", margin: "auto" }} class="form-group">
               <label for="formGroupExampleInput2">
-                <b>Date of Journey</b>
+                <b>City</b>
               </label>
               <input
-                type="date"
+                type="text"
+                value={city}
                 class="form-control"
                 id="formGroupExampleInput2"
-                placeholder="Date"
-                value={dateOfDeparture}
-                onChange={(e) => setDateOfDeparture(e.target.value)}
+                placeholder="City"
+                onChange={(e) => setCity(e.target.value)}
               />
             </div>
-            <div style={{ width: "50%" }} class="form-group">
-              <label for="formGroupExampleInput2">
-                <b>Date of Coming Back</b>
-              </label>
-              <input
-                type="date"
-                class="form-control"
-                id="formGroupExampleInput2"
-                placeholder="Date"
-                value={dateOfArrival}
-                onChange={(e) => setDateOfArrival(e.target.value)}
-              />
-            </div>
-          </div>
 
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">
+            <div style={{ margin: "auto", marginTop: "24px" }} className="row">
+              <div style={{ width: "50%" }} class="form-group">
+                <label for="formGroupExampleInput2">
+                  <b>Date of Journey</b>
+                </label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Date"
+                  value={dateOfDeparture}
+                  onChange={(e) => setDateOfDeparture(e.target.value)}
+                />
+              </div>
+              <div style={{ width: "50%" }} class="form-group">
+                <label for="formGroupExampleInput2">
+                  <b>Date of Coming Back</b>
+                </label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Date"
+                  value={dateOfArrival}
+                  onChange={(e) => setDateOfArrival(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">
+                {" "}
+                <b>Discription </b>
+              </label>
+              <textarea
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="4"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+            </div>
+            <div style={{ fontSize: "18px", marginTop: "10px" }}>
               {" "}
-              <b>Discription </b>
-            </label>
-            <textarea
-              class="form-control"
-              id="exampleFormControlTextarea1"
-              rows="4"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            ></textarea>
-          </div>
-          <div style={{ fontSize: "18px", marginTop: "10px" }}>
-            {" "}
-            {/* Current Members :-<b> Nalin, Prerit, Joshi</b>{" "} */}
-          </div>
-          <button type="button" class="btn btn-primary btn-sm btn-lg btn-block">
-            Create Group
-          </button>
+              {/* Current Members :-<b> Nalin, Prerit, Joshi</b>{" "} */}
+            </div>
+            <button class="btn btn-primary btn-sm btn-lg btn-block">
+              Create Group
+            </button>
+          </form>
         </div>
       </div>
     </div>
