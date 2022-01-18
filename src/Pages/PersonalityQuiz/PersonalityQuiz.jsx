148a012057736c7a5472ca8a0ccc1a65e0145daf
vijayjoshi16 <joshivijay324@gmail.com>
2022-01-18 22:45:01 +0530

    Connected personality quiz to backend

--- src/Pages/PersonalityQuiz/PersonalityQuiz.jsx
+++ src/Pages/PersonalityQuiz/PersonalityQuiz.jsx
@@ -1,12 +1,18 @@
 import { QuizContainer, SubmitButton } from "./PersonalityQuiz.styles.js";
 import { quizQuestions } from "./quizQuestions.js";
 import QuestionCard from "./QuestionCard/QuestionCard.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import Toast from "../../Components/Toast/toast.js";
 
 const PersonalityQuiz = () => {
   const [selectedOptions, setSelectedOptions] = useState([
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
   ]);
+  const [user, setUser] = useState(undefined);
+
+  const navigate = useNavigate();
 
   const updateChoice = (questionIndex, choiceIndex) => {
     const choiceList = selectedOptions;
@@ -17,11 +23,45 @@ const PersonalityQuiz = () => {
 
   const submitHandler = () => {
     if (selectedOptions.includes(-1)) {
-      console.log("Please answer all the questions!");
+      Toast("", "Please answer all the questions!", "", "");
       return;
     }
     console.log("Ok");
+    fetch("http://localhost:3001/api/user/addpersonalityquiz", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: user._id,
+        answers: selectedOptions,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.errors) {
+          Toast("", result.errors, "", "");
+        } else {
+          Toast("Successfully completed the quiz", "", "", "");
+          navigate("/home");
+        }
+      });
   };
+
+  useEffect(() => {
+    if (localStorage.getItem("token") === null) {
+      navigate("/login");
+    } else {
+      const decoded = jwt_decode(localStorage.getItem("token"));
+      if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      } else {
+        console.log(decoded);
+        setUser(decoded);
+      }
+    }
+  }, []);
 
   return (
     <QuizContainer>
