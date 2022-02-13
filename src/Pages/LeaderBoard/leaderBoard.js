b8e6a3232a0615e8580163a1427edc87617049d0
him-rane <himanshurane6501@gmail.com>
2022-02-14 02:52:26 +0530

    LeaderBoard Complete

--- src/Pages/LeaderBoard/leaderBoard.js
+++ src/Pages/LeaderBoard/leaderBoard.js
@@ -1,157 +1,109 @@
 import "./leaderBoard.scss";
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import rankone from "../LeaderBoard/one.png";
+import steps from "../LeaderBoard/podium.png";
 
 const LeaderBoard = () => {
+  const navigate = useNavigate();
   const [users, setUsers] = useState([]);
-
   useEffect(() => {
-    fetch(`http://localhost:3001/api/user/getAllUsers`, {
-      method: "get",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        //  console.log(data.users);
-        data.users.map((user) => {
-          let like = 0;
-          let comment = 0;
-          user.posts.map((post) => {
-            // console.log(post.postId.comments.length);
-            // console.log(post.postId.likes.length);
-            // console.log(post.postId.likes.length + post.postId.comments.length);
-            like += post.postId.likes.length;
-            comment += post.postId.comments.length;
-          });
-          // console.log(like + comment);
-          let count = like + comment + user.badges;
-          user.count = count;
+    if (localStorage.getItem("token") === null) {
+      navigate("/login");
+    } else if (localStorage.getItem("token") != "null") {
+      const decoded = jwt_decode(localStorage.getItem("token"));
+      if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      } else {
+        if (decoded) {
+          // setUser(decoded);
+          fetch("http://localhost:3001/api/user/getAllUsers", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: decoded._id,
+            }),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data.users);
+              // setPosts(data.allPosts.reverse());
+              data.users.map((user) => {
+                let like = 0;
+                let comment = 0;
+                user.posts.map((post) => {
+                  like += post.postId.likes.length;
+                  comment += post.postId.comments.length;
+                });
 
-          // like = 0;
-          // comment = 0;
-        });
-        data.users.sort((a, b) => {
-          return b.count - a.count;
-        });
+                let count = like + comment + user.badges;
+                user.count = count;
+              });
+              data.users.sort((a, b) => {
+                return b.count - a.count;
+              });
+              console.log(data.users);
 
-        setUsers(data.users);
-      });
+              setUsers(data.users);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }
+    } else {
+      navigate("/login");
+    }
   }, []);
-  console.log(users);
-
-  // console.log(users);
-  // users.map((user) => {
-  //   let like = 0;
-  //   let comment = 0;
-  //   user.posts.map((post) => {
-  //     // console.log(post.postId.comments.length);
-  //     // console.log(post.postId.likes.length);
-  //     // console.log(post.postId.likes.length + post.postId.comments.length);
-  //     like += post.postId.likes.length;
-  //     comment += post.postId.comments.length;
-  //   });
-  //   // console.log(like + comment);
-  //   let count = like + comment + user.badges;
-  //   user.count = count;
-
-  //   console.log(user);
-  //   like = 0;
-  //   comment = 0;
-  // });
 
   return (
-    <div className="l-wrapper">
-      <div className="c-header"></div>
-      <div className="l-grid">
-        <div className="l-grid__item l-grid__item--sticky">
-          {/* <div className="c-card u-bg--light-gradient u-text--dark">
-            <div className="c-card__body">
-              <div className="u-display--flex u-justify--space-between">
-                <div className="u-text--left">
-                  <div className="u-text--small">My Rank</div>
-                  <h2>3rd Place</h2>
-                </div>
-                <div className="u-text--right">
-                  <div className="u-text--small">My Score</div>
-                  <h2>24</h2>
-                </div>
-              </div>
+    <div className="container">
+      <div className="row">
+        <div className="brandimgbox col-2">
+          <img className="brandimg " src={`${steps}`} alt="" srcset="" />
+        </div>
+        <div className="lbbrand col-10">Explora Rank</div>
+      </div>
+      <hr></hr>
+      <div className="row">
+        <div className="col-4">
+          <div className="wcar">
+            <div className="wrankbadge">
+              <img className="wbadge" src={`${rankone}`} />
             </div>
-          </div> */}
+            <div className="wpoints"></div>
+            <div className="wimgbox">
+              <img className="wimg" src={`${users[0].picUrl}`} alt="" />
+            </div>
+            <div className="row">
+              <div className=" rankCard col-2">
+                <div className="score">Score</div>
+                <div className="wrank">{users[0].count}</div>
+              </div>
 
-          <div className="c-card">
-            <div className="c-card__body">
-              <div className="u-text--center" id="winner">
-                <img class="c-avatar c-avatar--lg" src={`${users[0].picUrl}`} />
-                <h3 class="u-mt--16">${users[0].name}</h3>
-                <span class="u-text--teal u-text--small">${users[0].name}</span>
-                <div className="c-card">
-                  <div className="c-card__body">
-                    <div className="u-text--center" id="winner"></div>
-                  </div>
-                </div>
+              <div className="winfo col-7">
+                <div className="wname ">{users[0].name}</div>
+                <div className="wusername ">{users[0].username}</div>
               </div>
             </div>
           </div>
         </div>
-        <div className="l-grid__item">
-          <div className="c-card">
-            <div className="c-card__header">
-              <h3>Received Kudos</h3>
-              <select className="c-select">
-                <option selected="selected">
-                  Sunday, Feb. 23 - Sunday, Feb. 30
-                </option>
-              </select>
+        <div className="col-8">
+          {users.map((user, index) => (
+            <div className="lbusers row ">
+              <div className="lbuserrank col-1">{index + 1}</div>
+              <div className="lbuserimgbox col-2">
+                <img className="lbuserimg" src={`${user.picUrl}`}></img>
+              </div>
+              <div className="lbnamebox col-6">
+                <div className="lbusername">{user.username}</div>
+                <div className="lbname">{user.name}</div>
+              </div>
+              <div className="lbuserpoints col-3">{user.count}</div>
             </div>
-            <div className="c-card__body">
-              <ul className="c-list" id="list">
-                <li className="c-list__item">
-                  <div className="c-list__grid">
-                    <div className="u-text--left u-text--small u-text--medium">
-                      Rank
-                    </div>
-                    <div className="u-text--left u-text--small u-text--medium">
-                      Team Member
-                    </div>
-                    <div className="u-text--right u-text--small u-text--medium">
-                      # of Kudos
-                    </div>
-                  </div>
-
-                  {users.map((element) => (
-                    <div key={element._id}>
-                      <div className="c-list__grid">
-                        <div className="c-flag c-place u-bg--transparent"></div>
-                        <div className="c-media">
-                          <img
-                            className="c-avatar c-media__img"
-                            src={`${element.picUrl}`}
-                          />
-                          <div className="c-media__content">
-                            <div className="c-media__title">
-                              {element.username}
-                            </div>
-                            <a
-                              className="c-media__link u-text--small"
-                              href={element.picUrl}
-                              target="_blank"
-                            >
-                              {element.name}
-                            </a>
-                          </div>
-                        </div>
-                        <div className="u-text--right c-kudos">
-                          <div className="u-mt--8">
-                            <strong>{element.count}</strong>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </li>
-              </ul>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
