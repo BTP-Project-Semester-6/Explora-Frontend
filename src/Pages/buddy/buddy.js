c2c3c34bf02e83a23b749d3f8e078bbd79dbf151
Prerit Kumar Jha <preritkrjha@gmail.com>
2022-03-12 12:45:58 +0530

    Each and Every routes connected to middleware

--- src/Pages/buddy/buddy.js
+++ src/Pages/buddy/buddy.js
@@ -45,6 +45,7 @@ export default function Buddy() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "x-auth-token": localStorage.getItem("token"),
       },
       body: JSON.stringify({
         id: user._id,
